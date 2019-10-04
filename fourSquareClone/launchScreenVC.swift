a0fe8c1fb44ed737638806b7b7ac7b3fdc1439af
Tutku Bide <>
2019-10-05 00:01:23 +0300

    first commit

---
+++ fourSquareClone/launchScreenVC.swift
@@ -0,0 +1,30 @@
+//
+//  launchScreenVC.swift
+//  fourSquareClone
+//
+//  Created by Tutku Bide on 4.07.2019.
+//  Copyright © 2019 Tutku Bide. All rights reserved.
+//
+
+import UIKit
+
+class launchScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ fourSquareClone/signIn.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fourSquareClone
+//
+//  Created by Tutku Bide on 28.06.2019.
+//  Copyright © 2019 Tutku Bide. All rights reserved.
+//
+
+import UIKit
+
+class signInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
